8f9546dede378948e9d3281ddad5f9156660ab0a
Rathi <rathiram7583@gmail.com>
2020-04-03 08:44:31 -0600

    Added

---
+++ main.js
@@ -0,0 +1,25 @@
+//BASIC CALCULATOR
+//Additon 
+var addition = function (a, b) {
+    var sum = (a + b);
+    console.log("The sum of two number is" + sum);
+};
+addition(3, 4);
+//SUBTRACTION
+var subtraction = function (a, b) {
+    var difference = (a - b);
+    console.log("The difference between two number is" + difference);
+};
+subtraction(10, 4);
+//multiplication
+var multiply = function (a, b) {
+    var multiply = (a * b);
+    console.log("The Product of two number is" + multiply);
+};
+multiply(14, 4);
+//Division
+var divide = function (a, b) {
+    var divide = (a / b);
+    console.log("The division of two number is" + divide);
+};
+divide(12, 4);
---
+++ main.ts
@@ -0,0 +1,37 @@
+
+
+//BASIC CALCULATOR
+//Additon 
+
+var addition=(a:number,b:number)=>{
+    var sum:number=(a+b);
+    console.log("The sum of two number is:"+sum);
+}
+
+addition(3,4);
+
+//SUBTRACTION
+var subtraction=(a:number,b:number)=>{
+    var difference:number=(a-b);
+    console.log("The difference between two number is:"+difference);
+}
+
+subtraction(10,4);
+
+
+//multiplication
+var multiply=(a:number,b:number)=>{
+    var multiply:number=(a*b);
+    console.log("The Product of two number is:"+multiply);
+}
+
+multiply(14,4);
+
+//Division
+var divide=(a:number,b:number)=>{
+    var divide:number=(a/b);
+    console.log("The division of two number is:"+divide);
+}
+
+divide(12,4);
+
